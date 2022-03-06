78f40719225bced1384bccb59ea99422cb2cc473
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2022-03-06 23:34:31 +0530

    changes for common loader component

--- src/app/loader.component.ts
+++ src/app/loader.component.ts
@@ -1,12 +1,9 @@
 import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-loader',
-  template: `<div class="overlay" *ngIf="visiblity">
-<mat-progress-spinner mode="indeterminate" [diameter]="200" *ngIf="visiblity" [style.margin-top.px] ="spinnerLocation"
-class="spinner-wrapper"
-[diameter]="100"></mat-progress-spinner>
-  </div>`
+  template: `<div class="overlay"><div class="center">  <mat-progress-spinner mode="indeterminate" *ngIf="visiblity" class="overlay spinner-wrapper"> </mat-progress-spinner></div></div>`
 })
+
 
 //  mode="indeterminate" class="d-inline-flex" *ngIf="IsWait">
 export class LoaderComponent  {
